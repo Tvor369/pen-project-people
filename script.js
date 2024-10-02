d1541df9db403987e2e39505d31f4545a5a53925
Trevor Stewart <146051077+Tvor369@users.noreply.github.com>
2024-10-02 16:35:28 +0000

    go live

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+// Get Stuff
+const thumbnail = document.querySelector('.thumbnail');
+const dialog = document.querySelector('dialog');
+
+// Do Stuff
+thumbnail.addEventListener( 'click' , () => {
+
+  dialog.showModal();
+  
+});
+
+dialog.addEventListener( 'click' , () => {
+
+  dialog.close();
+
+});
